3788b7d89a3d3924d91ed87afee8fbbf8b4d81b1
Han Lan Chen (Ted) <sonumber9@gmail.com>
2014-06-25 00:43:52 +1200

    Set Sound Profiles with Vibration

--- src/com/automates/automate/RoutinesFragment.java
+++ src/com/automates/automate/RoutinesFragment.java
@@ -4,34 +4,46 @@ import com.automates.automate.routines.actions.SoundProfiles;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class RoutinesFragment extends Fragment {
-	
+	private TextView testText;
+
 	public RoutinesFragment(){}
-	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_routines, container, false);
         
         
-    	final Button loginButton = (Button) rootView.findViewById(R.id.testButton);
-        loginButton.setOnClickListener(new OnClickListener() {
+        testText = (TextView) rootView.findViewById(R.id.testText);
+        
+    	final Button testButton = (Button) rootView.findViewById(R.id.testButton);
+    	testButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(final View v) {
                 testButton(v);
             }
         });
+        
+        
+        
         return rootView;
+        
+        
     }
 	
 	public void testButton(View v) {
-		SoundProfiles.setSilent(this.getActivity());
+//		SoundProfiles.setSoundProfile(this.getActivity(), SoundProfiles.NORMAL_NO_VIBRATE);
+		int m = SoundProfiles.getMode(this.getActivity());
+		testText.setText(Integer.toString(m));
 	}
 	
 
