c90a74db42bddc738ac417f80c9b7732b06371e0
Han Lan Chen (Ted) <sonumber9@gmail.com>
2014-07-02 22:15:07 +1200

    Write phonestate to log file

--- src/com/automates/automate/PhoneState.java
+++ src/com/automates/automate/PhoneState.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
+import android.os.Environment;
+import android.util.Log;
 
 import com.automates.automate.locations.GPSTracker;
 import com.automates.automate.routines.settings.*;
 
 // Singleton
 public final class PhoneState {
+	private static final String TAG = "PhoneState";
 	private static Location location;
 	private static boolean dataEnabled;
 	private static boolean wifiEnabled;
@@ -41,19 +44,45 @@ public final class PhoneState {
 	
 	public static void logIntent(Intent intent) {
 		String s = "";
+		String trigger = "";
+		if (intent.getAction().equals("android.media.RINGER_MODE_CHANGED")) {
+			trigger = "Ringer";
+		} else if (intent.getAction().equals("android.net.wifi.WIFI_STATE_CHANGED")) {
+			trigger = "Wifi";
+		} else if (intent.getAction().equals("android.net.conn.CONNECTIVITY_CHANGE")) {
+			trigger = "Wifi or Data";
+		}
+		s = trigger + "|" + soundProfile + "|" + wifiEnabled + "|" + dataEnabled + "|" + location;
 		
-		s = s + soundProfile + "|" + wifiEnabled + "|" + dataEnabled + "|" + location;
+		if (isExternalStorageWritable()) {
+			Log.d(TAG, s);
+			appendLog(s);
+		} else {
+			Log.d(TAG, "external not writable");
+		}
 		
 	}
 	
 	public static void logLocation() {
+		String s = "Location" + "|" + soundProfile + "|" + wifiEnabled + "|" + dataEnabled + "|" + location;
+		
+		appendLog(s);
 		
 	}
 	
+	/* Checks if external storage is available for read and write */
+	private static boolean isExternalStorageWritable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state)) {
+	        return true;
+	    }
+	    return false;
+	}
 	
-	public void appendLog(String text)
-	{       
-	   File logFile = new File("sdcard/log.file");
+	private static void appendLog(String text)
+	{  
+	String root = Environment.getExternalStorageDirectory().toString();
+	   File logFile = new File(root + "/log.file");
 	   if (!logFile.exists())
 	   {
 	      try
