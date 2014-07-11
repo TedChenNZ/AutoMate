305a2cdfe8eb64d6c6ece3006a5389df59a4a839
Han Lan Chen (Ted) <sonumber9@gmail.com>
2014-07-11 17:24:45 +1200

    Update wifi to show BSSID instead of true

--- src/com/automates/automate/PhoneState.java
+++ src/com/automates/automate/PhoneState.java
@@ -1,5 +1,4 @@
 package com.automates.automate;
-import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
@@ -15,146 +14,148 @@ import com.automates.automate.sqlite.SQLiteDBManager;
 
 // Singleton
 public final class PhoneState {
-    private static final String TAG = "PhoneState";
-    private static Location location;
-    private static boolean dataEnabled;
-    private static boolean wifiEnabled;
-    private static int soundProfile;
-    private static SQLiteDBManager db;
-    private static int time = 0;
-    private static GPSTracker gpsTracker;
-    private static List<UserLocation> locationsList;
-    
-    private static PhoneState instance = null;
-    private PhoneState() {
-	// Exists only to defeat instantiation.
-	// Set to private -> no subclassing
-    }
-    public static PhoneState getInstance() {
-	if(instance == null) {
-	    instance = new PhoneState();
-	}
-	return instance;
-    }
+	private static final String TAG = "PhoneState";
+	private static Location location;
+	private static boolean dataEnabled;
+	private static boolean wifiEnabled;
+	private static int soundProfile;
+	private static SQLiteDBManager db;
+	private static int time = 0;
+	private static GPSTracker gpsTracker;
+	private static List<UserLocation> locationsList;
+	private static String wifiBSSID;
 
-    public static void update(Context context) {
-    	if (db == null) {
-    		db = new SQLiteDBManager(context);
-    	}
-    	if (locationsList == null) {
-    		locationsList = new UserLocationsList(context);
-    	}
+	private static PhoneState instance = null;
+	private PhoneState() {
+		// Exists only to defeat instantiation.
+		// Set to private -> no subclassing
+	}
+	public static PhoneState getInstance() {
+		if(instance == null) {
+			instance = new PhoneState();
+		}
+		return instance;
+	}
+
+	public static void update(Context context) {
+		if (db == null) {
+			db = new SQLiteDBManager(context);
+		}
+		if (locationsList == null) {
+			locationsList = new UserLocationsList(context);
+		}
 		soundProfile = SoundProfiles.getMode(context);
 		wifiEnabled = Wifi.getWifiEnabled(context);
 		dataEnabled = Data.getDataEnabled(context);
+		wifiBSSID = Wifi.getWifiBSSID(context);
 		if (gpsTracker == null) {
 			gpsTracker = new GPSTracker(context);
 		}
 		location = gpsTracker.getLocation();
 		if (time == 0) {
-		    time = (int) System.currentTimeMillis();
+			time = (int) System.currentTimeMillis();
 		}
 
-    }
-
-    public static String checkConnectivityIntent() {
-	String s = Logger.getLastLine();
-	if (s != null) {
-	    String[] split = s.split("\\|");
-	    if (split.length < 5) {
-		Log.d(TAG,"length error");
-	    } else {
-		String wifi = split[3];
-		String data = split[4];
-		if (!data.equals(String.valueOf(dataEnabled))) {
-		    // dataChange = true;
-		    return "Data";
-
-		} else if (!wifi.equals(String.valueOf(wifiEnabled))) {
-		    // wifiChange = true;
-		    return "Wifi";
-		}
-	    }
 	}
-	return null;
-    }
 
-    public static String getEvent(Intent intent) {
-	String event = "";
-	if (intent.getAction().equals("android.media.RINGER_MODE_CHANGED")) {
-	    event = "Ringer";
-	} else if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-	    event = "Bootup";
-	} else if (intent.getAction().equals("android.net.wifi.WIFI_STATE_CHANGED")) {
-	    event = "Wifi";
-	} else if (intent.getAction().equals("android.net.conn.CONNECTIVITY_CHANGE")) {
-	    event = checkConnectivityIntent();
-	    if (event == null) {
+	public static String checkConnectivityIntent() {
+		String s = Logger.getLastLine();
+		if (s != null) {
+			String[] split = s.split("\\|");
+			if (split.length < 5) {
+				Log.d(TAG,"length error");
+			} else {
+				String wifi = split[3];
+				String data = split[4];
+				if (!data.equals(String.valueOf(dataEnabled))) {
+					// dataChange = true;
+					return "Data";
+
+				} else if (!wifi.equals(String.valueOf(wifiBSSID))) {
+					// wifiChange = true;
+					return "Wifi";
+				}
+			}
+		}
 		return null;
-	    } else {
-		//				Log.d(TAG, event);
-	    }
 	}
-	Log.d(TAG, "Event: " + event);
-	return event;
-    }
 
-    public static String getEventAction(String event) {
-	String action = "";
-	if (event.equals("Wifi")) {
-	    action = String.valueOf(wifiEnabled);
-	} else if (event.equals("Data")) {
-	    action = String.valueOf(dataEnabled);
-	} else if (event.equals("Ringer")) {
-	    action = String.valueOf(soundProfile);
+	public static String getEvent(Intent intent) {
+		String event = "";
+		if (intent.getAction().equals("android.media.RINGER_MODE_CHANGED")) {
+			event = "Ringer";
+		} else if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+			event = "Bootup";
+		} else if (intent.getAction().equals("android.net.wifi.WIFI_STATE_CHANGED")) {
+			event = "Wifi";
+		} else if (intent.getAction().equals("android.net.conn.CONNECTIVITY_CHANGE")) {
+			event = checkConnectivityIntent();
+			if (event == null) {
+				return null;
+			} else {
+				//				Log.d(TAG, event);
+			}
+		}
+		Log.d(TAG, "Event: " + event);
+		return event;
 	}
-	return action;
-    }
 
-    public static void logIntent(String event) {
-	String s = "";
-	if (event != null) {
-
-
-	    String t = "" + time;
-
-	    s = event + "|" + t + "|" + soundProfile + "|" + wifiEnabled + "|" + dataEnabled + "|" + location;
-	    Logger.appendLog(s);
+	public static String getEventAction(String event) {
+		String action = "";
+		if (event.equals("Wifi")) {
+			action = String.valueOf(wifiBSSID);
+		} else if (event.equals("Data")) {
+			action = String.valueOf(dataEnabled);
+		} else if (event.equals("Ringer")) {
+			action = String.valueOf(soundProfile);
+		}
+		return action;
 	}
-    }
 
-    //	public static void logLocation() {
-    //		Time now = new Time();
-    //		now.setToNow();
-    //		String time = now.format("%H%M%S");
-    //		String s = "Location" + "|" + time + "|" +soundProfile + "|" + wifiEnabled + "|" + dataEnabled + "|" + location;
-    //		Logger.appendLog(s);
-    //	}
+	public static void logIntent(String event) {
+		String s = "";
+		if (event != null) {
 
 
-    public static String getSetLocation() {
-	return location.toString();
-    }
+			String t = "" + time;
 
-    // Getters
-    public static Location getLocation() {
-	return location;
-    }
-    public static boolean isDataEnabled() {
-	return dataEnabled;
-    }
-    public static boolean isWifiEnabled() {
-	return wifiEnabled;
-    }
-    public static int getSoundProfile() {
-	return soundProfile;
-    }
-    public static int getTime() {
-	return time;
-    }
-    public static SQLiteDBManager getDb() {
-        return db;
-    }
+			s = event + "|" + t + "|" + soundProfile + "|" + wifiBSSID + "|" + dataEnabled + "|" + location;
+			Logger.appendLog(s);
+		}
+	}
+
+	//	public static void logLocation() {
+	//		Time now = new Time();
+	//		now.setToNow();
+	//		String time = now.format("%H%M%S");
+	//		String s = "Location" + "|" + time + "|" +soundProfile + "|" + wifiEnabled + "|" + dataEnabled + "|" + location;
+	//		Logger.appendLog(s);
+	//	}
+
+
+	public static String getSetLocation() {
+		return location.toString();
+	}
+
+	// Getters
+	public static Location getLocation() {
+		return location;
+	}
+	public static boolean isDataEnabled() {
+		return dataEnabled;
+	}
+	public static boolean isWifiEnabled() {
+		return wifiEnabled;
+	}
+	public static int getSoundProfile() {
+		return soundProfile;
+	}
+	public static int getTime() {
+		return time;
+	}
+	public static SQLiteDBManager getDb() {
+		return db;
+	}
 	public static GPSTracker getGPSTracker() {
 		return gpsTracker;
 	}
@@ -163,6 +164,9 @@ public final class PhoneState {
 	}
 	public static void setLocationsList(List<UserLocation> locationsList) {
 		PhoneState.locationsList = locationsList;
+	}
+	public static String getWifiBSSID() {
+		return wifiBSSID;
 	}
 
 }
--- src/com/automates/automate/routines/settings/Wifi.java
+++ src/com/automates/automate/routines/settings/Wifi.java
@@ -1,9 +1,11 @@
 package com.automates.automate.routines.settings;
 
 import android.content.Context;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
 public class Wifi {
+	
 	public Wifi() {}
 	
 	public static void setWifiEnabled(Context context, boolean enabled) {
@@ -14,6 +16,18 @@ public class Wifi {
 	public static boolean getWifiEnabled(Context context) {
 		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE); 
 		return wifiManager.isWifiEnabled();
+	}
+	
+	public static String getWifiBSSID(Context context) {
+		WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+		WifiInfo info = wifiManager.getConnectionInfo();
+		String BSSID = "false";
+		if (info.getBSSID() != null) {
+			BSSID = info.getBSSID();
+		}
+		
+		return BSSID;
+		
 	}
 
 }
