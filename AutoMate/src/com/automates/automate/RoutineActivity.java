53755f699eb9f16cc000c54dbeb2624eb89aeef5
Han Lan Chen (Ted) <sonumber9@gmail.com>
2014-07-27 23:34:55 +1200

    Added button in RoutinesActivity

--- AutoMate/src/com/automates/automate/RoutineActivity.java
+++ AutoMate/src/com/automates/automate/RoutineActivity.java
@@ -1,12 +1,28 @@
 package com.automates.automate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class RoutineActivity extends FragmentActivity {
+	
+	private Button saveButton;
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_activity);
+        
+        // Setting click event listener for the add button
+        saveButton = (Button) findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new OnClickListener() {
+        	
+            @Override
+            public void onClick(View v) {
+            	// Do things
+            }
+		});
 	}
 }
