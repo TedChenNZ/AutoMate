7cc24381afba37b580b770ef45097c2ba35af3d7
Han Lan Chen (Ted) <sonumber9@gmail.com>
2014-07-31 11:11:27 +1200

    Routine activate commented

--- AutoMate/src/com/automates/automate/routines/Routine.java
+++ AutoMate/src/com/automates/automate/routines/Routine.java
@@ -71,8 +71,8 @@ public class Routine {
 	// TODO Auto-generated method stub
 	//ask user if location/wifi/data are conditions NOTIFICATION
 	//alarm manager to do something for time based.
-	RoutineActivity r = new RoutineActivity();
-	r.notification("Pattern recognised", "AutoMate has recognised a pattern - tap on this notification to configure the routine!", this.id);
+//	RoutineActivity r = new RoutineActivity();
+//	r.notification("Pattern recognised", "AutoMate has recognised a pattern - tap on this notification to configure the routine!", this.id);
 
     }
 
