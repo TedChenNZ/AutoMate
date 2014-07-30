32150069fec8d6873d6155f834254d9e59e113df
Dhanish Semar <msem006@aucklanduni.ac.nz>
2014-07-30 23:13:23 +1200

    half attempt at notif builder

--- AutoMate/src/com/automates/automate/RoutineActivity.java
+++ AutoMate/src/com/automates/automate/RoutineActivity.java
@@ -5,9 +5,12 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
+import android.widget.ImageView;
 
 public class RoutineActivity extends FragmentActivity {
 
@@ -45,5 +48,29 @@ public class RoutineActivity extends FragmentActivity {
 	notification.flags += Notification.FLAG_ONGOING_EVENT;
 	notification.flags += Notification.FLAG_AUTO_CANCEL;
 	mNotificationManager.notify(NOT_ID, notification);
+	
+//	String ns = Context.NOTIFICATION_SERVICE;
+//	NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+//	
+//	int NOT_ID = 1;
+//	
+//	int requestID = (int) System.currentTimeMillis();
+//	Notificationt.Builder notification = new Notification.Builder(this)
+//        .setContentTitle(subject)
+//        .setContentText(body)
+//        .setTicker("Automate - Pattern recognised!")
+//        .setSmallIcon(R.drawable.ic_routines)
+//        .setLargeIcon(BitmapFactory.decodeResource(this.getResources(),
+//                R.drawable.ic_routines))
+//        .build();
+//	Context context = getApplicationContext();
+//	Intent notificationIntent = new Intent(this, RoutineActivity.class);
+//	notificationIntent.putExtra("routineID", id);
+//	PendingIntent contentIntent = PendingIntent.getActivity(this, requestID, notificationIntent, 0);
+//	//notification.setLatestEventInfo(context, subject, body, contentIntent);
+//	notification.setContentIntent(contentIntent);
+//	notification.flags += Notification.FLAG_ONGOING_EVENT;
+//	notification.flags += Notification.FLAG_AUTO_CANCEL;
+//	mNotificationManager.notify(NOT_ID, notification);
     }
 }
