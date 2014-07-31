9d211453bf93f5eedb3a545e6250333169cae929
Han Lan Chen (Ted) <sonumber9@gmail.com>
2014-07-31 12:06:16 +1200

    TimelyChecker null check

--- AutoMate/src/com/automates/automate/routines/TimelyChecker.java
+++ AutoMate/src/com/automates/automate/routines/TimelyChecker.java
@@ -13,13 +13,18 @@ public class TimelyChecker extends BroadcastReceiver{
     @Override
     public void onReceive(Context context, Intent intent) {
 	// TODO Auto-generated method stub
-	Log.d("test", "in timely checker");
+	Log.d("TimelyChecker", "in timely checker");
 	Time time = new Time();
 	time.setToNow();
-	Log.d("test", "Time is " + time.hour + ":" + time.minute + ", day is " + time.weekDay);
+	Log.d("TimelyChecker", "Time is " + time.hour + ":" + time.minute + ", day is " + time.weekDay);
 	//PhoneState.getRoutineApplier().checkRoutines();
 	RoutineApplier rA = PhoneState.getRoutineApplier();
-	rA.checkRoutines();
+	if (rA != null) {
+		rA.checkRoutines();
+	} else {
+		Log.d("TimelyChecker", "rA is null");
+	}
+	
     }
 
 }
