ad02fd5b007c51fa975f97f6c65c1f08265999ba
Dhanish Semar <msem006@aucklanduni.ac.nz>
2014-08-11 22:30:44 +1200

    bug check fix

--- AutoMate/src/com/automates/automate/settings/IntentReceiver.java
+++ AutoMate/src/com/automates/automate/settings/IntentReceiver.java
@@ -12,24 +12,34 @@ import com.automates.automate.pattern.PatternControl;
 import com.automates.automate.pattern.PatternController;
 
 public class IntentReceiver extends BroadcastReceiver {	
-	private final static String TAG = "IntentReceiver";
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		Log.d(TAG, intent.getAction());
-		PhoneState.update(context);
-		String event = PhoneState.getEvent(intent);
-		
-		SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-		boolean learning = sharedPrefs.getBoolean("pref_learning", true);
-		
-		if (event != null && learning) {
-			Log.d("IntentReceiver", "event: " + event);
-			String eventAction = PhoneState.getEventAction(event);
-			Log.d("IntentReciever", "eventAction: " + eventAction);
-			PatternControl pg = new PatternController(event,eventAction);
-			pg.updateDatabase();
-		}
+    private final static String TAG = "IntentReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+	Log.d(TAG, intent.getAction());
+	PhoneState.update(context);
+	String event = PhoneState.getEvent(intent);
 
+	SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+	boolean learning = sharedPrefs.getBoolean("pref_learning", true);
+
+	if (event != null && learning) {
+	    Log.d("IntentReceiver", "event: " + event);
+	    String eventAction = PhoneState.getEventAction(event);
+	    Log.d("IntentReceiver", "eventAction: " + eventAction);
+	    if(!randomBug(event, eventAction)){
+		PatternControl pg = new PatternController(event,eventAction);
+		pg.updateDatabase();
+	    }
 	}
+
+    }
+    private boolean randomBug(String event, String eventAction) {
+	if(event.equalsIgnoreCase(Settings.MDATA) && eventAction.equalsIgnoreCase("true")){
+	    if(PhoneState.getWifiBSSID().equals("true")){
+		return true;
+	    }
+	}
+	return false;
+    }
 
 }
